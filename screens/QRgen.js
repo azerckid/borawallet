3ab724e5e736cfdaf9034c2d0f2fc4ac074d700e
zizimoos_iMac <zizimoos@gmail.com>
2021-03-24 23:34:56 +0900

    qrcode gen

--- screens/QRgen.js
+++ screens/QRgen.js
@@ -1,29 +1,25 @@
 import React, { useState } from "react";
 import { TouchableOpacity, TextInput, View, Text } from "react-native";
 import { Button, Input } from "react-native-elements";
-import QRCode from "react-native-qrcode";
+import QRCode from "react-native-qrcode-svg";
 import firebase from "../firebase/fire";
 
 const QRgen = ({ navigation }) => {
   const [text, setText] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+ 
   const [error, setError] = useState("");
 
   return (
-    <View>
+    <View style={{flex:1}}>
       <TextInput
         style={{ height: 40 }}
         placeholder="Type Address!"
         onChangeText={(text) => setText(text)}
         defaultValue={text}
       />
-      <Button title="QR code generator" onPress={() => generateQrCode()} />
-      {imageUrl ? <QRCode value={text} size={200}></QRCode> : null}
-      {/* {imageUrl ? (
-        <a href={imageUrl} download>
-          <img src={imageUrl} alt="img" />
-        </a>
-      ) : null} */}
+     
+      {text ? <QRCode value={text} size={200}></QRCode> : null}
+  
     </View>
   );
 };
